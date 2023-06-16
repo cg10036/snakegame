0d64496345067e8dc13df90ee0905d766ee09fc3
cg10036 <cg10036@gmail.com>
2023-06-16 23:33:02 +0900

    fix: errors

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main() {
+void run() {
     Game game = Game(); // Game 클래스 생성
     for (int i = 1; i <= 4; i++) {
         try {
@@ -14,9 +14,10 @@ int main() {
             i--; // 해당 레벨을 다시 실행
         }
     }
-    game.Game::~Game(); // 소멸자 명시적 실행
-    setvbuf(stdout, NULL, _IOLBF, 0);
-    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+int main() {
+    run();
     cout << "Game Clear!" << endl;
     cout << "나는 이무기가 아니었다!" << endl;
 }
