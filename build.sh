93d49921c11b0249ec35bdd59e8f1cfb6bd1de53
cg10036 <cg10036@gmail.com>
2023-06-16 22:07:22 +0900

    add: build.sh cmake

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# build with cmake
+#cmake .
+#make
+
+# build with only g++
 g++ board.h game.cpp game.h input.cpp input.h items.cpp items.h main.cpp snake.cpp snake.h -lncursesw -o snakegame
