041999921f34c172179a4a2df8e176fe8ef1caf1
cg10036 <cg10036@gmail.com>
2023-06-16 23:39:44 +0900

    fix: errors

--- main.cpp
+++ main.cpp
@@ -3,21 +3,18 @@
 
 using namespace std;
 
-void run() {
-    Game game = Game(); // Game 클래스 생성
+int main() {
+    Game *game = new Game(); // Game 클래스 생성
     for (int i = 1; i <= 4; i++) {
         try {
             // 레벨별 게임 실행
-            game.run(i, {4, 1, 1, 1, 1, 10});
+            game->run(i, {4, 1, 1, 1, 1, 10});
         } catch (...) {
             // 죽었을 경우
             i--; // 해당 레벨을 다시 실행
         }
     }
-}
-
-int main() {
-    run();
+    delete game;
     cout << "Game Clear!" << endl;
     cout << "나는 이무기가 아니었다!" << endl;
 }
